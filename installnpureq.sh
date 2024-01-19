bd5ed6701259bf6d1364c2dfae4444a675f4576f
StarrySky0208 <136553778+zzc0208@users.noreply.github.com>
2024-01-19 08:30:02 +0800

    Create installnpureq.sh

---
+++ installnpureq.sh
@@ -0,0 +1,4 @@
+wget 'https://gitee.com/ascend/pytorch/releases/download/v5.0.rc3-pytorch2.1.0/torch_npu-2.1.0rc1-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl'
+pip install torch==2.1.0
+pip install torch_npu-2.1.0rc1-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
+pip install -r req.txt
